7a6381fa7b51df08daefa8d4d2ae2e56ab7a9b65
Selçuk Korkmaz <selcukorkmaz@hotmail.com>
2017-01-30 18:35:00 +0200

    update

--- server.R
+++ server.R
@@ -2863,7 +2863,7 @@ shinyServer(function(input, output, session){
     
     if(input$FourMethodConsensus){
       
-      datatable(df, escape=FALSE,  rownames=FALSE, class = 'cell-border hover stripe', extensions = c('Buttons'), options = list(dom = 'T<"clear">lfrtip',buttons = c('copy', 'excel', 'pdf'),  columnDefs = list(list(targets = c(9:12), visible = FALSE)))
+      datatable(df, escape=FALSE,  rownames=FALSE, class = 'cell-border hover stripe', extensions = c('Buttons'), options = list(dom = 'T<"clear">lfrtip',buttons = c('copy', 'excel', 'pdf'),  columnDefs = list(list(targets = c(8:11), visible = FALSE)))
                 
       )%>% formatStyle(
         columns = c("Consensus", "Sequence Cluster", "PISA", "EPPIC", "Text Mining"), valueColumns = c("ResultC","ResultSC", "ResultP", "ResultE", "ResultTM"),
